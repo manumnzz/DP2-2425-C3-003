c79d1ce281502f30059a3a0cc214a18c3e183b93
60Manu82 <manlavcor@alum.us.es>
2025-03-12 18:34:13 +0100

    G-007: Create Service instance

---
+++ src/main/java/acme/entities/service.java
@@ -0,0 +1,53 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Pattern;
+
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class service {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@Pattern(regexp = "^(https?|ftp)://.*$", message = "Must be a valid URL")
+	@Column(unique = true)
+	private String				imageLink;
+
+	@Mandatory
+	@DecimalMin(value = "0.0", inclusive = false)
+	@Automapped
+	private double				time;
+
+	@Optional
+	@ValidString(pattern = "^[A-Z]{4}-[0-9]{2}$")
+	@Automapped
+	private String				promotionCode;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				discountAmount;
+}
