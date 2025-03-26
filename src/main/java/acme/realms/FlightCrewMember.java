b2f5dae9d9c361afe22605922e03ea9aecda790b
manu.mnzz <manbuzmun@alum.us.es>
2025-03-26 13:40:55 +0100

    The csv totally fixed

--- src/main/java/acme/realms/FlightCrewMember.java
+++ src/main/java/acme/realms/FlightCrewMember.java
@@ -35,7 +35,7 @@ public class FlightCrewMember extends AbstractRole {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
 	@Column(unique = true)
 	private String				employeeCode;
 
